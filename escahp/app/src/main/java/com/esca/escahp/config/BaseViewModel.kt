fd83030e8441d039af4133bfbd7ec554e9653e56
DyeonKim <vlxjdus@gmail.com>
2022-11-19 00:00:04 +0900

    chore : add BaseViewModel

---
+++ escahp/app/src/main/java/com/esca/escahp/config/BaseViewModel.kt
@@ -0,0 +1,18 @@
+package com.esca.escahp.config
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel() {
+    private val _isLoading = MutableLiveData<Boolean>(false)
+    val isLoading: LiveData<Boolean> get() = _isLoading
+
+    protected fun showProgress() {
+        _isLoading.value = true
+    }
+
+    protected fun hideProgress() {
+        _isLoading.value = false
+    }
+}
