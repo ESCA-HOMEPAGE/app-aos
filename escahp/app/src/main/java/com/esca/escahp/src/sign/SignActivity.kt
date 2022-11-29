9a9f406064385d3903829afd087bfd965551dd37
DyeonKim <vlxjdus@gmail.com>
2022-11-29 20:41:10 +0900

    chore : change package architecture

--- escahp/app/src/main/java/com/esca/escahp/src/sign/SignActivity.kt
+++ escahp/app/src/main/java/com/esca/escahp/src/sign/SignActivity.kt
@@ -1,19 +1,20 @@
-package com.esca.escahp.src.main
+package com.esca.escahp.src.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.esca.escahp.R
 import com.esca.escahp.config.BaseActivity
-import com.esca.escahp.databinding.ActivityMainBinding
+import com.esca.escahp.databinding.ActivitySignBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
+class SignActivity : BaseActivity<ActivitySignBinding>() {
     override val TAG: String
-        get() = "MainActivity"
+        get() = "SignActivity"
     override val layoutRes: Int
-        get() = R.layout.activity_main
+        get() = R.layout.activity_sign
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 }
